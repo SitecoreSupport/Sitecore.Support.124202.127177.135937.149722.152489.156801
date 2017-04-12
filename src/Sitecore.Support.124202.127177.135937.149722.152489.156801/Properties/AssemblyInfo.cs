f856a18ce11888e8a49a1eb747aae7d06fffdb0f
valeriyleman <vle@sitecore.net>
2017-04-12 12:14:25 +0300

    Initial fix

---
+++ src/Sitecore.Support.124202.127177.135937.149722.152489.156801/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sitecore.Support.124202.127177.135937.149722.152489.156801")]
+[assembly: AssemblyProduct("Sitecore.Support.124202.127177.135937.149722.152489.156801")]
+[assembly: ComVisible(false)]
